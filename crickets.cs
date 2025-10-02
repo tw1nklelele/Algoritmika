58addb01cf098949de0d7364b2ee7a6c9df200d5
tw1nklelele <159630024+tw1nklelele@users.noreply.github.com>
2025-10-03 00:27:21 +0300

    Create crickets.cs

---
+++ crickets.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    // Update вызывается каждый кадр
+    void Update()
+    {
+        // Выводим слово "crickets" в консоль каждый кадр
+        Debug.Log("crickets");
+    }
+}
